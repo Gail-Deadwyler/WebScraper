b5d88063a482a66d8ffa8b2170c29424bdea46d4
rivendell <webwyler@gmail.com>
2019-10-19 14:32:53 -0500

    added models and public folders, configure middleware(app.use)

--- server.js
+++ server.js
@@ -7,10 +7,21 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-//define PORT
-var PORT = 3000;
+//define PORT 3000 or use Heroku PORT
+var PORT = process.env.PORT || 3000;
 
 //intialize Express;
 var app = express()
 
 //Configure Middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+
+// Make public a static folder
+app.use(express.static("public"));
+
