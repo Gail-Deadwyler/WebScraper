0ddc42626c56150d900fa6d0bed7b42c98133d44
rivendell <webwyler@gmail.com>
2019-10-19 16:36:12 -0500

    tested the /scrape route - results logged to the console

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var app = express()
 app.use(logger("dev"));
 
 // Parse request body as JSON
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Make public a static folder
@@ -37,48 +37,65 @@ mongoose.connect(MONGODB_URI);
 
 // A GET route for scraping the chicago tribune website
 
-app.get("/scrape", function(request,response) {
+app.get("/", function(request, response) {
 
-// First, grab the body of the html with axios
+    console.log("JuJu Team");
 
-// First, tell the console what server.js is doing
-console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from chicago tribune website:" +
-            "\n***********************************\n");
+    //response.send("JuJu Team!!");
 
-axios.get("http://www.chicagotribune.com").then(function (response){
+});
 
-// Load the Response into cheerio and save it to a variable
-var $ = cheerio.load(response.data);
+// app.get("/", function(req, res) {
+//     res.send("Hello world");
+//   });
 
-// An empty array to save the data that we'll scrape
-var results = [];
+app.get("/scrape", function (request, response) {
 
- // With cheerio, find each h2-tag with the "regular-text-mobile.h6" class
-  // (i: iterator. element: the current element)
-$("h2.regular-text-mobile.h6").each(function(i, element) {
+    // First, grab the body of the html with axios
 
-    // Save the text of the element in a "title" variable
-    var title = $(element).text();
+    // First, tell the console what server.js is doing
+    console.log("\n***********************************\n" +
+        "Grabbing every thread name and link\n" +
+        "from chicago tribune website:" +
+        "\n***********************************\n");
 
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).children().attr("href");
+    axios.get("http://www.chicagotribune.com").then(function (response) {
 
-// Save these results in an object that we'll push into the results array we defined earlier
-    results.push({
-        title: title,
-        link: link
+        // Load the Response into cheerio and save it to a variable
+        var $ = cheerio.load(response.data);
+
+        // An empty array to save the data that is scraped
+        var results = [];
+
+        // With cheerio, find each h2-tag with the "regular-text-mobile.h6" class
+        // (i: iterator. element: the current element)
+        $("h2.regular-text-mobile.h6").each(function (i, element) {
+
+            // Save the text of the element in a "title" variable
+            var title = $(element).text();
+
+            // In the currently selected element, look at its child elements (i.e., its a-tags),
+            // then save the values for any "href" attributes that the child elements may have
+            var link = $(element).children().attr("href");
+
+            // Save these results in an object that will be pushed into the results array defined earlier
+            results.push({
+                title: title,
+                link: link
+            });
+
+        });
+
+        console.log(results);
+
     });
 
 });
 
-console.log(results);
-
-})
-
-})
+//Start the server
+app.listen(PORT, function () {
+    console.log("App is listening on " + PORT);
+});
 
 
 
