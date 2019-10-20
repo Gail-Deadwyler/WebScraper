ff95714bb9225f7f7b7b4d65430d0949d4fa5516
rivendell <webwyler@gmail.com>
2019-10-20 10:17:24 -0500

    added ajax call to /scrape route in app.js and comment change to server.js

--- public/app.js
+++ public/app.js
@@ -1,8 +1,14 @@
+// Start Scrape from the Chicago Tribune 
+$.ajax({
+    method: "GET",
+    url: "/scrape"
+  }).then(console.log("scrape complete"));
+
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
+      // Display the information on the page
       $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
     }
   });
